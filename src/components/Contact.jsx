a95d6f87c73525d45987e02a65370574aa45572a
Natnael-Tewodros <natnaeltewodros03@gmail.com>
2024-08-21 22:23:05 +0300

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import { FaEnvelope, FaMapMarkedAlt, FaPhone } from "react-icons/fa";
 
 const Contact = () => {
   return (
-    <div className="bg-black text-white py-20" id="contact">
+    <div className="bg-black text-white py-20" id="Contact">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">Contact Me</h2>
         <div className="flex flex-col md:flex-row items-center md:space-x-12">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,29 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="bg-black text-white px-8 md:px-16 lg:px-24">
-      <div className="container py-2 flex justify-center md:justify-between  items-center">
-        <div className="text-2xl  font-bold hidden  md:inline">Natnael</div>
-        <div className="space-x-6">
-          <a href="#Home" className="hover:text-grey-400">
+      <div className="container py-2 flex justify-center md:justify-between items-center">
+        <div className="text-2xl font-bold hidden md:inline">Natnael</div>
+        <div className="space-x-8">
+          {" "}
+          {/* Adjusted spacing between links */}
+          <a href="#Home" className="hover:text-gray-400">
             Home
           </a>
-          <a href="#About " className="hover:text-grey-400">
+          <a href="#About" className="hover:text-gray-400">
+            {" "}
+            {/* Removed extra space */}
             About Me
           </a>
-          <a href="#Skills" className="hover:text-grey-400">
+          <a href="#Skills" className="hover:text-gray-400">
             Skills
           </a>
-          <a href="#Projects" className="hover:text-grey-400">
+          <a href="#Projects" className="hover:text-gray-400">
             Projects
           </a>
-          <a href="#Contact" className="hover:text-grey-400">
+          <a href="#Contact" className="hover:text-gray-400">
             Contact
           </a>
         </div>
-        <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">
-          Contact Me
-        </button>
       </div>
     </nav>
   );
